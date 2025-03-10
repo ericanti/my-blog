28158936bffc8adc39a0ed5507cbd774204adca0
Eric Antillon <ericantilloncharles@gmail.com>
2025-03-10 16:03:53 -0600

    fixed code

--- post2/test1.py
+++ post2/test1.py
@@ -216,19 +216,15 @@ for df in [billboard_df_1969, billboard_df_2019]:
             # sleep for 3 seconds
             time.sleep(3)
 
-billboard_df_2019 = pd.read_csv('C:/Users/erica/STAT386/blog/another-stat386-theme/post2/billboard2019_features.csv')
-billboard_df_1969 = pd.read_csv('C:/Users/erica/STAT386/blog/another-stat386-theme/post2/billboard1969_features.csv')
-
-# fill any duplicates with null values first
+# fill any duplicates that have null features
 features = ['danceability', 'genre', 'gender', 'mood',
             'instrumental', 'bpm', 'key', 'loudness', 'mood_happy']
 
 for df in [billboard_df_1969, billboard_df_2019]:
-    # Create mask for rows needing imputation
-    needs_imputation = df['danceability'].isna()
+    needs_features = df['danceability'].isna()
     
-    # Group by MBID and propagate features forward
-    df.loc[needs_imputation, features] = df.groupby('mbid')[features].transform(
+    # Group by MBID and propagate features forward and backwards
+    df.loc[needs_features, features] = df.groupby('mbid')[features].transform(
         lambda x: x.ffill().bfill()
     )
 
