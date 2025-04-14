d7bc71245b75ba08c608c9ec4c90afd376a20838
Eric Antillon <ericantilloncharles@gmail.com>
2025-04-14 12:32:04 -0600

    started post 3

---
+++ post2/post_3_code.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+# load data
+billboard2019 = pd.read_csv('billboard2019.csv')
+billboard1969 = pd.read_csv('billboard1969.csv')
